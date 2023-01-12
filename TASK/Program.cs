4a51d6a4d3fbbf6f2ba18794725d5513cfef10db
Вадим <118062456+DeveloperVadim@users.noreply.github.com>
2023-01-12 22:34:49 +0700

    TASK

---
+++ TASK/Program.cs
@@ -0,0 +1,27 @@
+﻿void ChangeArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string[4] {"hello", "2", "world", ":-)"};
+PrintArray(array);
+string[] array2 = new string[array.Length];
+ChangeArray(array, array2);
+PrintArray(array2);
